a177df043d65fd58f21fc86e5fa18047b79a905c
rhafik-gonzalez-ifood <rhafik.gonzalez@ifood.com.br>
2019-10-03 07:37:48 -0300

    general fixes

--- ui/start.sh
+++ ui/start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 
-/configure.sh ${ZOOKEEPER_SERVICE_HOST:-$1} ${NIMBUS_SERVICE_HOST:-$2}	CONFIG="$STORM_CONF_DIR/storm.yaml"
+CONFIG="$STORM_CONF_DIR/storm.yaml"
 echo "Zookeeper: $ZOOKEEPER_SERVICE_HOST"
 echo "Nimbus: $NIMBUS_SERVICE_HOST"
 
 
-exec bin/storm ui	cat << EOF > "$CONFIG"
+cat << EOF > "$CONFIG"
 storm.zookeeper.servers: ["$ZOOKEEPER_SERVICE_HOST"]
 nimbus.seeds: ["$NIMBUS_SERVICE_HOST"]
 EOF
