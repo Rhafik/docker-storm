f2e3e159b7cf47936f5eda87047070eae7a7e5b4
rhafik-gonzalez-ifood <rhafik.gonzalez@ifood.com.br>
2019-10-03 07:32:06 -0300

    general fixes

--- nimbus/start.sh
+++ nimbus/start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
 CONFIG="$STORM_CONF_DIR/storm.yaml"
-
 echo $ZOOKEEPER_SERVICE_HOST
-exec bin/storm nimbus	cat << EOF > "$CONFIG"
+cat << EOF > "$CONFIG"
 storm.zookeeper.servers: ["$ZOOKEEPER_SERVICE_HOST"]
 storm.local.hostname: $NIMBUS_SERVICE_HOST
 storm.local.dir: "/tmp"
