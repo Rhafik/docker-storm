14ac27aacf160b3831830d7680c3a7d218e52195
rhafik-gonzalez-ifood <rhafik.gonzalez@ifood.com.br>
2019-10-07 09:36:48 -0300

    General fixes

--- nimbus/start.sh
+++ nimbus/start.sh
@@ -5,7 +5,6 @@ echo $ZOOKEEPER_SERVICE_HOST
 cat << EOF > "$CONFIG"
 storm.zookeeper.servers: ["$ZOOKEEPER_SERVICE_HOST"]
 storm.local.hostname: $NIMBUS_SERVICE_HOST
-storm.nimbus.seeds: ["$NIMBUS_SERVICE_HOST"]
 storm.local.dir: "/tmp"
 nimbus.thrift.max_buffer_size: 500000
 EOF
